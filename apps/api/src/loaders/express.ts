dff8a61b90bfb0701f394e02d1b5573354ce2c4f
Logan Martlew <logan.martlew@gmail.com>
2022-09-29 16:23:08 +1300

    Added ApiResponse type

--- apps/api/src/loaders/express.ts
+++ apps/api/src/loaders/express.ts
@@ -1,36 +1,35 @@
-// import { ApiResponse } from '@zoom-conference-manager/api-interfaces';
-// import { ApiError } from '@zoom-conference-manager/errors';
-import { Express } from 'express';
+import { ApiError } from '@gensymtech-projects/errors';
+import { Express, Request, Response, NextFunction } from 'express';
 import { json, urlencoded } from 'body-parser';
 import cors from 'cors';
 import getRouter from '../app/router';
-// import routes from '../routes';
-// import { Logger } from './logger';
+import { Logger } from './logger';
+import { ApiResponse } from '@gensymtech-projects/api-interfaces';
 
 export default (app: Express) => {
-  // app.use((req: Request, res: Response, next: NextFunction) => {
-  //   try {
-  //     next();
-  //   } catch (error) {
-  //     Logger.error(error);
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    try {
+      next();
+    } catch (error) {
+      Logger.error(error);
 
-  //     let apiError: ApiError;
+      let apiError: ApiError;
 
-  //     if (error instanceof ApiError) {
-  //       apiError = error;
-  //     } else {
-  //       apiError = new ApiError(error, 1000, null);
-  //     }
+      if (error instanceof ApiError) {
+        apiError = error;
+      } else {
+        apiError = new ApiError(error, 1000, null);
+      }
 
-  //     const response: ApiResponse<never> = {
-  //       status: apiError.statusCode,
-  //       message: apiError.message,
-  //       error: apiError,
-  //     };
+      const response: ApiResponse<never> = {
+        status: apiError.statusCode,
+        message: apiError.message,
+        error: apiError,
+      };
 
-  //     res.status(apiError.statusCode).json(response);
-  //   }
-  // });
+      res.status(apiError.statusCode).json(response);
+    }
+  });
 
   app.use(cors());
   app.use(json());
@@ -38,24 +37,24 @@ export default (app: Express) => {
 
   app.use(getRouter());
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  // app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
-  //   Logger.error(error);
+  //eslint-disable-next-line @typescript-eslint/no-unused-vars
+  app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+    Logger.error(error);
 
-  //   let apiError: ApiError;
+    let apiError: ApiError;
 
-  //   if (error instanceof ApiError) {
-  //     apiError = error;
-  //   } else {
-  //     apiError = new ApiError(error, 1000, null);
-  //   }
+    if (error instanceof ApiError) {
+      apiError = error;
+    } else {
+      apiError = new ApiError(error, 1000, null);
+    }
 
-  //   const response: ApiResponse<never> = {
-  //     status: apiError.statusCode,
-  //     message: apiError.message,
-  //     error: apiError,
-  //   };
+    const response: ApiResponse<never> = {
+      status: apiError.statusCode,
+      message: apiError.message,
+      error: apiError,
+    };
 
-  //   res.status(apiError.statusCode).json(response);
-  // });
+    res.status(apiError.statusCode).json(response);
+  });
 };
--- libs/api-interfaces/src/lib/api-interfaces.ts
+++ libs/api-interfaces/src/lib/api-interfaces.ts
@@ -1,3 +1,20 @@
-export interface Message {
-  message: string;
-}
+import { ApiError } from '@gensymtech-projects/errors';
+
+export type ApiResponse<T> =
+  | {
+      status: number;
+      message: string;
+      data?: T;
+      error?: never;
+    }
+  | {
+      status: number;
+      message: string;
+      data?: never;
+      error: ApiError;
+    };
+
+export type Controller<T> = (req: Request) => Promise<ApiResponse<T>>;
+export type ExtractControllerData<T> = T extends Controller<infer U>
+  ? U
+  : never;
