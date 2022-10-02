7371a5711c44d006eaa848e2bd30a1de4c88eb61
Logan Martlew <logan.martlew@gmail.com>
2022-10-03 12:57:53 +1300

    Added route protection

--- apps/api/src/app/router.ts
+++ apps/api/src/app/router.ts
@@ -1,6 +1,7 @@
 import { Controller } from '@gensymtech-projects/api-interfaces';
 import express from 'express';
 import AuthController from '../features/auth/auth.controller';
+import { requireAuth } from '../features/auth/auth.middleware';
 import ProjectController from '../features/projects/project.controller';
 import { Logger } from '../loaders/logger';
 import formatResponse from './formatResponse';
@@ -16,12 +17,14 @@ type Route =
   | {
       path: string;
       method?: never;
+      protected?: never;
       handler?: never;
       subroutes: Route[];
     }
   | {
       path: string;
       method: Method;
+      protected: boolean;
       handler: Controller<unknown>;
       subroutes?: never;
     };
@@ -36,31 +39,37 @@ const routes: Route[] = [
           {
             path: '',
             method: Method.POST,
+            protected: true,
             handler: ProjectController.create,
           },
           {
             path: '',
             method: Method.GET,
+            protected: false,
             handler: ProjectController.findAll,
           },
           {
             path: 'move',
             method: Method.PUT,
+            protected: true,
             handler: ProjectController.move,
           },
           {
             path: ':id',
             method: Method.GET,
+            protected: false,
             handler: ProjectController.findOne,
           },
           {
             path: ':id',
             method: Method.PUT,
+            protected: true,
             handler: ProjectController.update,
           },
           {
             path: ':id',
             method: Method.DELETE,
+            protected: true,
             handler: ProjectController.delete,
           },
         ],
@@ -71,11 +80,13 @@ const routes: Route[] = [
           {
             path: 'login',
             method: Method.POST,
+            protected: false,
             handler: AuthController.login,
           },
           {
             path: 'logout',
             method: Method.POST,
+            protected: false,
             handler: AuthController.logout,
           },
         ],
@@ -91,7 +102,16 @@ const getRouter = () => {
     const path = [...currentPaths, route.path].join('/') || '/';
 
     if (route.handler) {
-      router[route.method](`${path}`, formatResponse(route.handler));
+      if (route.protected) {
+        router[route.method](
+          `${path}`,
+          requireAuth,
+          formatResponse(route.handler)
+        );
+      } else {
+        router[route.method](`${path}`, formatResponse(route.handler));
+      }
+
       Logger.info(
         `ROUTER: Route ${route.method.toUpperCase()} ${path} assigned`
       );
