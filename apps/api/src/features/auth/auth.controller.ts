dcfc2b9375aaa910fbb24905a63fbc3f988a1d8b
Logan Martlew <logan.martlew@gmail.com>
2022-10-04 17:15:14 +1300

    Set cookie secure to true

--- apps/api/src/features/auth/auth.controller.ts
+++ apps/api/src/features/auth/auth.controller.ts
@@ -14,12 +14,14 @@ export default class AuthController {
       httpOnly: true,
       maxAge: 1000 * 60 * 15,
       sameSite: 'none',
+      secure: true,
     });
 
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
       maxAge: 1000 * 60 * 60 * 24 * 7,
       sameSite: 'none',
+      secure: true,
     });
 
     return {
@@ -34,12 +36,14 @@ export default class AuthController {
       httpOnly: true,
       maxAge: 0,
       sameSite: 'none',
+      secure: true,
     });
 
     res.cookie('refreshToken', '', {
       httpOnly: true,
       maxAge: 0,
       sameSite: 'none',
+      secure: true,
     });
 
     return {
